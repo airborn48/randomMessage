6e55b199b326d5c083a929877e1b50fdc2a10b09
airborm <4ayushrana@gmail.com>
2022-01-06 01:43:28 -0800

    see what will happen

--- codeAcademy/randomMessage.js
+++ codeAcademy/randomMessage.js
@@ -14,3 +14,6 @@ console.log(`You ${verb} ${number} ${object}`)
 console.log('This be the new branch palis')
 
 console.log('this my third branch')
+
+console.log('I just merged all branches but what happens if I keep typing on third branch')
+console.log('will it change master branch hmmm??')
